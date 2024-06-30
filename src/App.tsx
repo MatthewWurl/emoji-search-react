feff23fda78ab70c1ffdf971c5b67fb08d89d042
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2024-06-30 19:47:53 -0400

    Add h1 and input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-const App = () => {
-  return <></>;
+import "./App.css";
+import { FC, useState } from "react";
+
+const App: FC = () => {
+  const [_, setEmojiQuery] = useState("");
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmojiQuery(event.target.value);
+  };
+
+  return (
+    <div className="container">
+      <h1>Emoji Search 🔎</h1>
+      <input
+        type="text"
+        placeholder="Search for an emoji..."
+        onChange={handleQueryChange}
+      />
+    </div>
+  );
 };
 
 export default App;
